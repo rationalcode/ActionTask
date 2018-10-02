7e3637c428b5e2aed138cba653bea8287b1b9852
rationalcode <marat_pochta@mail.ru>
2018-10-02 17:50:16 +0500

    add animation

--- app/src/main/java/com/example/admin/actiontask/MainActivity.java
+++ app/src/main/java/com/example/admin/actiontask/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.admin.actiontask;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView imageView = findViewById(R.id.imageView);
+
+        new Handler().postDelayed(new Runnable() {
+
+            // Using handler with postDelayed called runnable run method
+
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, FirstScreen.class);
+                startActivity(i);
+
+                Animation rotateAnimation = new RotateAnimation(0,180);
+                imageView.startAnimation(rotateAnimation);
+
+                // close this activity
+                finish();
+            }
+        }, 5*1000); // wait for 5 seconds
     }
 }
