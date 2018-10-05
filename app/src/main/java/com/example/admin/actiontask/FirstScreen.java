86c933700f8bc7bee05c0869ed71ac586091ef84
rationalcode <marat_pochta@mail.ru>
2018-10-05 16:27:55 +0500

    rotate imageview

--- app/src/main/java/com/example/admin/actiontask/FirstScreen.java
+++ app/src/main/java/com/example/admin/actiontask/FirstScreen.java
@@ -6,17 +6,27 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import io.reactivex.Completable;
-import io.reactivex.CompletableObserver;
 import io.reactivex.Observable;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.operators.completable.CompletableFromAction;
 
-public class FirstScreen extends AppCompatActivity {
+public class FirstScreen extends AppCompatActivity implements Runnable{
 
-
+    private static final String TAG = "log" ;
+    static float width;
+    static float height;
+    ImageView imageView;
+    static RotateAnimation rotate;
+    DisplayMetrics metrics;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,35 +34,71 @@ public class FirstScreen extends AppCompatActivity {
 
         setContentView(R.layout.first_screen);
 
+        imageView = findViewById(R.id.imageView);
 
-        RotateStartView();
-       // startMainActivity();
+        width = imageView.getX()/2;
+        height = imageView.getY()/2;
+        metrics = this.getResources().getDisplayMetrics();
+
+
+        Log.d(TAG, width+" "+height);
+
+        final Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                RotateStartView(width,height);
+
+
+            }
+
+        });
+
+            Thread t2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            });
+
+        t1.start();
+        t2.start();
+
+        //RotateStartView();
+        //startMainActivity();
 
 
     }
 
-    private void RotateStartView (){
-
-        RotateAnimation rotate;
+    private void RotateStartView (float width, float height){
 
 
-            DisplayMetrics metrics = this.getResources().getDisplayMetrics();
-            int width = metrics.widthPixels / 2;
-            int height = metrics.heightPixels/2;
 
-            rotate = new RotateAnimation(0, 360, width, height);
-            rotate.setDuration(5000);
-            //rotate.setRepeatCount(1);
 
-            final ImageView imageView = findViewById(R.id.imageView);
 
+
+//            int width = metrics.widthPixels / 2;
+//            int height = metrics.heightPixels/2;
+
+//            rotate = new RotateAnimation(0, 180, width,height);
+//            rotate.setDuration(5000);
+//            rotate.setInterpolator(new LinearInterpolator());
+
+
+        final Animation rotate = AnimationUtils.loadAnimation(
+                this, R.anim.rotate_action);
 
 
             imageView.startAnimation(rotate);
-
-
-            //Toast.makeText(getApplicationContext(),rotate.hasEnded()+"",Toast.LENGTH_LONG).show();
-
 
     }
 
@@ -74,6 +120,11 @@ public class FirstScreen extends AppCompatActivity {
                 finish();
             }
         },5000 ); // wait for 5 seconds
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
